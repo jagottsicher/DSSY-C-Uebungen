fbc2b84e77756c4eb2445a1e674058509783454c
Jens Schendel <30713344+jagottsicher@users.noreply.github.com>
2022-02-16 14:11:09 +0800

    Create calc-ext.c

---
+++ 01-Taschenrechner/Beipiele/calc-ext.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main () {
+  float x,y,z;
+  char operand[2];
+  char line[BUFFERSIZE];
+  
+  if ( fgets(line, BUFFSIZ,stdin) && sscanf(line,"%f%1s%f", &x,operand,&y) == 3 ) {
+    switch ( operand[0] ) {
+      case '+': z = x + y;
+      break;
+      case '-': z = x - y;
+      break;
+      case '*': z = x * y;
+      break;
+      case '/': z = x / y; // Achtung: division durch 0 muss abgefangen werden!
+      break;
+      default: fputs("Fehlerhafter oder unbekannter Operator!\n"; 
+      return 1;
+     }
+   printf("%f %s %f = %f\n",x,operand,y,z);
+  } else {
+    fputs("Fehlerhafte Eingabe!\n", stderr);
+    return 0;
+  } 
+}
