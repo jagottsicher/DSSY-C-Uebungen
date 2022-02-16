bc8dd20b7d7d3c96f09a5fb937ac267d2a838f91
Jens Schendel <30713344+jagottsicher@users.noreply.github.com>
2022-02-16 13:45:20 +0800

    Create calc-sdt.c

---
+++ 01-Taschenrechner/Beipiele/calc-sdt.c
@@ -0,0 +1,67 @@
+/*
+ * Übungsaufgabe 
+ * Aufgabe: Schreibe einen einfachen Taschenrechner
+ * 
+*/
+
+// benutzt printf/scanf
+#include <stdio.h>
+
+// Startpunkt der Anwendung
+int main(int argc, char** argv)
+{
+  // Erzeuge Variablen für die Ein- und Ausgabe
+  int ersteZahl = 0;
+  char operation = 0;
+  int zweiteZahl = 0;
+  int ergebnis = 0;
+ 
+  // Frage nach den Zahlen
+  // hier könnte natürlich auch eine kleine Schleife auswerten, ob die eingabe überhaupt eine Zahl ist.
+  printf("Wie lautet die erste Zahl?        ");
+  scanf("%d\n",&ersteZahl);
+  
+  printf("Welche Operation wird gewünscht?  ");
+  scanf("%c",&operation);
+  
+  printf("Wie lautet die andere Zahl?       ");
+  scanf("%d",&zweiteZahl);
+  
+  // berechne das Ergebnis (suche passende operation)
+  switch( operation )
+  {
+  case '+':
+    ergebnis=ersteZahl+zweiteZahl;
+    break;
+  case '-':
+    ergebnis=ersteZahl-zweiteZahl;
+    break;
+  case '*':
+    ergebnis=ersteZahl*zweiteZahl;
+    break;
+  case '/':
+    ergebnis=ersteZahl/zweiteZahl;
+    break;
+  case '%':
+    ergebnis=ersteZahl%zweiteZahl;
+    break;
+  case '|':
+    ergebnis=ersteZahl|zweiteZahl;
+    break;
+  case '&':
+    ergebnis=ersteZahl&zweiteZahl;
+    break;
+  case '^':
+    ergebnis=ersteZahl^zweiteZahl;
+    break;
+  default:
+    fprintf(stderr, "\'%c\' ist keine gültige Eingabe\n");
+    return -1;
+  }
+  
+  printf("----------------------------------------\n");
+  printf("Ergebnis für %d%c%d                  %d\n", ersteZahl, operation, zweiteZahl, ergebnis);
+  
+  
+  return 0;
+}
