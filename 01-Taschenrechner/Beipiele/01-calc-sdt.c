349d150d9e3da2a27d55d53d71a6c0d786e6dc69
Jens Schendel <30713344+jagottsicher@users.noreply.github.com>
2022-03-02 10:16:43 +0800

    Create 01-calc-sdt.c

--- 01-Taschenrechner/Beipiele/01-calc-sdt.c
+++ 01-Taschenrechner/Beipiele/01-calc-sdt.c
@@ -10,44 +10,52 @@
 // Startpunkt der Anwendung
 int main()
 {
-  // Erzeuge Variablen für die Ein- und Ausgabe
-  int ersteZahl = 0;
-  char operation = 0;
-  int zweiteZahl = 0;
-  int ergebnis = 0;
+    char weiter = 'j';
+      while (weiter == 'j')
+      {
+          // Erzeuge Variablen für die Ein- und Ausgabe
+          int ersteZahl = 0;
+          char operation = 0;
+          int zweiteZahl = 0;
+          int ergebnis = 0;
 
-  // Frage nach den Zahlen
-  // hier könnte natürlich auch eine kleine Schleife auswerten, ob die eingabe überhaupt eine Zahl ist.
-  printf("Wie lautet die erste Zahl?        ");
-  scanf("%d%*c",&ersteZahl);
-  printf("Welche Operation wird gewünscht?  ");
-  scanf("%c%*c",&operation);
-  printf("Wie lautet die andere Zahl?       ");
-  scanf("%d%*c",&zweiteZahl);
+          // Frage nach den Zahlen
+          // hier könnte natürlich auch eine kleine Schleife auswerten, ob die eingabe überhaupt eine Zahl ist.
+          printf("Wie lautet die erste Zahl?        ");
+          scanf("%d%*c",&ersteZahl);
+          printf("Welche Operation wird gewünscht?  ");
+          scanf("%c%*c",&operation);
+          printf("Wie lautet die andere Zahl?       ");
+          scanf("%d%*c",&zweiteZahl);
 
-  // berechne das Ergebnis (suche passende operation)
-  switch( operation )
-  {
-  case '+':
-    ergebnis=ersteZahl+zweiteZahl;
-    break;
-  case '-':
-    ergebnis=ersteZahl-zweiteZahl;
-    break;
-  case '*':
-    ergebnis=ersteZahl*zweiteZahl;
-    break;
-  case '/':
-    ergebnis=ersteZahl/zweiteZahl;
-    break;
-  default:
-    fprintf(stderr, "\'%c\' ist keine gültige Eingabe.\n");
-    return -1;
-  }
+          // berechne das Ergebnis (suche passende operation)
+          switch( operation )
+          {
+          case '+':
+            ergebnis = ersteZahl + zweiteZahl;
+            break;
+          case '-':
+            ergebnis = ersteZahl - zweiteZahl;
+            break;
+          case '*':
+            ergebnis = ersteZahl * zweiteZahl;
+            break;
+          case '/':
+            ergebnis = ersteZahl / zweiteZahl;
+            break;
+          case '%':
+            ergebnis = ersteZahl % zweiteZahl;
+            break;
+          default:
+            fprintf(stderr, "Das ist keine gültige Eingabe.\n\n");
+            return -1;
+          }
 
-  printf("----------------------------------------\n");
-  printf("Ergebnis für %d%c%d                  %d\n", ersteZahl, operation, zweiteZahl, ergebnis);
-
-
+          printf("----------------------------------------\n");
+          printf("Ergebnis für %d %c %d =             %d\n\n", ersteZahl, operation, zweiteZahl, ergebnis);
+          printf("Nochmals probieren? (j/n)         ");
+          scanf("%c%*c", &weiter);
+          printf("\n");
+      }
   return 0;
 }
