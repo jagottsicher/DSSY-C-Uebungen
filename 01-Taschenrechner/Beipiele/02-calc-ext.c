8d9b6b6f641d7f019e6f914c21a7565e1da3b17c
Jens Schendel <30713344+jagottsicher@users.noreply.github.com>
2022-03-09 14:41:15 +0800

    Update 02-calc-ext.c

--- 01-Taschenrechner/Beipiele/02-calc-ext.c
+++ 01-Taschenrechner/Beipiele/02-calc-ext.c
@@ -12,37 +12,52 @@
 
 int main () {
 
-  float x,y,ergebnis;
+  float x, y, ergebnis;
 
   char operand[2];
-  char line[BUFFERSIZE];
+  char line[BUFFERSIZE]; // Das ist ein String: ein array von Zeichen (Char)
 
-  printf("Aufgabe eingeben. <ENTER> für Berechnung => ");
+  while (1)
+  {
+    printf("Aufgabe eingeben, dann [ENTER] fuer Berechnung => ");
 
-  // scanf reads from the standard input stream stdin.
-  // fscanf reads from the named input stream.
-  // sscanf reads from the character string s.
-  // Each function reads characters, interprets them according to a format,
-  // and stores the results in its arguments. Each expects, as arguments,
-  // a control string format described below, and a set of pointer arguments
-  // indicating where the converted input should be stored.
+    // scanf reads from the standard input stream stdin.
+    // fscanf reads from the named input stream.
+    // sscanf reads from the character string s.
+    // Each function reads characters, interprets them according to a format,
+    // and stores the results in its arguments. Each expects, as arguments,
+    // a control string format described below, and a set of pointer arguments
+    // indicating where the converted input should be stored.
 
-  if ( fgets(line, BUFFERSIZE,stdin) && sscanf(line,"%f%1s%f", &x,operand,&y) == 3 ) {
-    switch ( operand[0] ) {
-      case '+': ergebnis = x + y;
-      break;
-      case '-': ergebnis = x - y;
-      break;
-      case '*': ergebnis = x * y;
-      break;
-      case '/': ergebnis = x / y; // Achtung: division durch 0 muss abgefangen werden!
-      break;
-      default: fputs("Fehlerhafter oder unbekannter Operator!\n", stdout);
-      return 1;
-     }
-   printf("%f %s %f = %f\n",x,operand,y,ergebnis);
-  } else {
-    fputs("Fehlerhafte Eingabe!\n", stderr);
-    return 0;
-  }
+    if ( fgets(line, BUFFERSIZE,stdin) && sscanf(line,"%f%1s%f", &x, operand, &y) == 3 )
+    {
+        switch ( operand[0] )
+        {
+            case '+': ergebnis = x + y;
+            break;
+            case '-': ergebnis = x - y;
+            break;
+            case '*': ergebnis = x * y;
+            break;
+            case '/': ergebnis = x / y; // Achtung: division durch 0 muss abgefangen werden!
+            break;
+            default: fputs("Fehlerhafter oder unbekannter Operator!\n", stdout);
+            return 1;
+         }
+        printf("%f %s %f = %f\n\n", x, operand, y, ergebnis);
+
+        // Gleiche Ausgabe, aber Ausgabe auf zwei Stellen nach dem Komma gerundet
+        // Hinweis: die Ausgabe der Werte ist gerundet, nicht die Inhalte der Variablen selbst!
+        printf("%.2f %s %.2f = %.2f\n\n", x, operand, y, ergebnis);
+
+        }
+        else
+        {
+            fputs("Fehlerhafte Eingabe!\n", stderr);
+            return -1;
+        }
+    }
+
+return 0;
 }
+
